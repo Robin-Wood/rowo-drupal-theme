b586914e53593f2cb5ac3414a65ca11fed9c22d8
Gustav Pursche <pursche@posteo.de>
2016-10-15 21:08:26 +0200

    article: add mixin for breaking outer paddings

--- templates/views-view-fields--schwerpunkte-zum-thema.tpl.php
+++ templates/views-view-fields--schwerpunkte-zum-thema.tpl.php
@@ -1,9 +1,18 @@
-<div class="row">
-  <div class="col-sm-4">
-    <div class="hero-slider-item_image"><?php print $fields["field_bild"]->content;?></div>
+<div class="focus-list__item">
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="focus-list__item-image-container">
+        <?php print $fields["field_bild"]->content;?>
+      </div>
+      </div>
+    <div class="col-sm-8">
+      <h3 class="focus-list__item-title">
+        <?php print $fields["title"]->content;?>
+      </h3>
+
+      <div class="focus-list__item-teaser">
+        <?php print $fields["field_teaser_text"]->content;?>
+      </div>
     </div>
-  <div class="col-sm-8">
-    <div class="hero-slider-item_title"><?php print $fields["title"]->content;?></div>
-    <div class="hero-slider-item_text"><?php print $fields["field_teaser_text"]->content;?></div>
   </div>
 </div>
