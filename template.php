46a0eae43cccba01f53ab5db43b7f596e03442e5
Tim Christensen <tim.christensen@robinwood.de>
2019-01-29 15:05:45 +0100

    adapt version

--- template.php
+++ template.php
@@ -8,7 +8,9 @@ function tweme_css_alter(&$css) {
   $exclude = array(
     'modules/system/system.menus.css' => FALSE,
     '//cdn.jsdelivr.net/bootstrap/3.3.5/css/bootstrap.css' => FALSE,
+    '//cdn.jsdelivr.net/bootstrap/3.4.0/css/bootstrap.css' => FALSE,
     'sites/all/themes/bootstrap/css/3.3.5/overrides.min.css' => FALSE,
+    'sites/all/themes/bootstrap/css/3.4.0/overrides.min.css' => FALSE,
     'misc/ui/jquery.ui.core.css' => FALSE,
     'sites/all/modules/views/css/views.css' => FALSE,
     'modules/node/node.css' => FALSE,
