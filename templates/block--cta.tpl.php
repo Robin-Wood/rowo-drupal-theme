97307da9688e13da010558bb9383c90a6ee02ab8
Tim Christensen <tim.christensen@robinwood.de>
2019-02-06 13:41:27 +0100

    styling cta

--- templates/block--cta.tpl.php
+++ templates/block--cta.tpl.php
@@ -1,2 +1,6 @@
-<div class="test">
-</div>
+<section class="test">
+  <?php print $fields['title']->content; ?>
+  <?php print $fields['field_bild']->content; ?>
+  <?php print $fields['field_kurzer_text']->content; ?>
+  <?php print $fields['field_button_text']->content; ?>
+</section>
