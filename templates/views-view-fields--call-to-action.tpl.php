e49131d5af03d28bd10d4a89adb63a59b14f8621
Tim Christensen <tim.christensen@robinwood.de>
2019-02-08 12:14:55 +0100

    cta section

--- templates/views-view-fields--call-to-action.tpl.php
+++ templates/views-view-fields--call-to-action.tpl.php
@@ -1,9 +1,18 @@
-<section class="test">
-  <a class="outer-link" href="https://www.robinwood.de">
-    <div class="teaser-slider__item equal-height-children">
-      <div class="teaser-slider__item-image-container">
-        <?php print $fields["field_bild"]->content; ?>
-      </div>
+<a class="outer-link" href="https://www.robinwood.de">
+  <div class="teaser-slider__item equal-height-children">
+    <div class="teaser-slider__item-image-container">
+      <?php print $fields["field_bild"]->content; ?>
     </div>
-  </a>
-</section>
+    <div class="teaser-slider__item-content-container teaser-slider__item-content-container--<?php print($modifier); ?>">
+      <h2 class="teaser-slider__item-title">
+        <?php print $fields["title"]->content; ?>
+      </h2>
+      <p class="teaser-slider__item-text">
+        <?php print $fields['field_kurzer_text']->content; ?>
+      </p>
+      <a href="<?php print $fields["field_link"]->content; ?>" class="button button--window button--is-centered">
+        <?php print $fields["field_button_text"]->content; ?>
+      </a>
+    </div>
+  </div>
+</a>
