62478d7f9edd7b1c390f3c9b52667182d1ac3af8
Tim Christensen <tim.christensen@robinwood.de>
2019-02-08 12:01:02 +0100

    cta section

---
+++ templates/views-view-fields--call-to-action.tpl.php
@@ -0,0 +1,9 @@
+<section class="test">
+  <a class="outer-link" href="https://www.robinwood.de">
+    <div class="teaser-slider__item equal-height-children">
+      <div class="teaser-slider__item-image-container">
+        <?php print $fields["field_bild"]->content; ?>
+      </div>
+    </div>
+  </a>
+</section>
