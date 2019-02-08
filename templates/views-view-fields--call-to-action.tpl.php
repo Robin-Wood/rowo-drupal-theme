a9ccbdbbcee6921d86c03c16f907df454d97cec7
Tim Christensen <tim.christensen@robinwood.de>
2019-02-08 12:52:54 +0100

    cta section

--- templates/views-view-fields--call-to-action.tpl.php
+++ templates/views-view-fields--call-to-action.tpl.php
@@ -1,18 +1,20 @@
-<a class="outer-link" href="https://www.robinwood.de">
-  <div class="teaser-slider__item equal-height-children">
-    <div class="teaser-slider__item-image-container">
-      <?php print $fields["field_bild"]->content; ?>
+<article>
+  <a class="outer-link" href="https://www.robinwood.de">
+    <div class="teaser-slider__item equal-height-children">
+      <div class="teaser-slider__item-image-container">
+        <?php print $fields["field_bild"]->content; ?>
+      </div>
+      <div class="teaser-slider__item-content-container teaser-slider__item-content-container--<?php print($modifier); ?>">
+        <h2 class="teaser-slider__item-title">
+          <?php print $fields["title"]->content; ?>
+        </h2>
+        <p class="teaser-slider__item-text">
+          <?php print $fields['field_kurzer_text']->content; ?>
+        </p>
+        <a href="https://www.robinwood.de" class="button button--window button--is-centered">
+          <?php print $fields["field_button_text"]->content; ?>
+        </a>
+      </div>
     </div>
-    <div class="teaser-slider__item-content-container teaser-slider__item-content-container--<?php print($modifier); ?>">
-      <h2 class="teaser-slider__item-title">
-        <?php print $fields["title"]->content; ?>
-      </h2>
-      <p class="teaser-slider__item-text">
-        <?php print $fields['field_kurzer_text']->content; ?>
-      </p>
-      <a href="<?php print $fields["field_link"]->content; ?>" class="button button--window button--is-centered">
-        <?php print $fields["field_button_text"]->content; ?>
-      </a>
-    </div>
-  </div>
-</a>
+  </a>
+</article>
