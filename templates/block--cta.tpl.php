78c8841fdb0893a3fa4588f97e36d092cae4783e
Tim Christensen <tim.christensen@robinwood.de>
2019-02-08 10:46:57 +0100

    cta section

--- templates/block--cta.tpl.php
+++ templates/block--cta.tpl.php
@@ -1,12 +1,9 @@
-<section class="test">
+<section id="<?php print $block_html_id; ?>" class="test">
   <a class="outer-link" href="https://www.robinwood.de">
     <div class="teaser-slider__item equal-height-children">
       <div class="teaser-slider__item-image-container">
-        <?php print $content['field_bild']; ?>
       </div>
-      <?php print $content['title']; ?>
-      <?php print $content['field_kurzer_text']; ?>
-      <?php print $content['field_button_text']; ?>
+      <?php print $content ?>
     </div>
   </a>
 </section>
