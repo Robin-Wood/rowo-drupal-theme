a7c358d66a6a832dbe54d8afe778836bbf6e62a5
Tim Christensen <tim.christensen@robinwood.de>
2019-02-08 12:00:51 +0100

    cta section

--- templates/block--cta.tpl.php
+++
@@ -1,9 +0,0 @@
-<section id="<?php print $block_html_id; ?>" class="test">
-  <a class="outer-link" href="https://www.robinwood.de">
-    <div class="teaser-slider__item equal-height-children">
-      <div class="teaser-slider__item-image-container">
-        <?php dpm(get_defined_vars()); ?>
-      </div>
-    </div>
-  </a>
-</section>
--- templates/node--call_to_action.tpl.php
+++
@@ -1,2 +0,0 @@
-<div class="node-test">
-</div>
