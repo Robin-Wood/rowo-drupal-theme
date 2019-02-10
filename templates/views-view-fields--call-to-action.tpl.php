48b1fd09d4983bf16ed3cf13febcd176c3d12967
Tim Christensen <tim.christensen@robinwood.de>
2019-02-10 11:23:50 +0100

    style cta

--- templates/views-view-fields--call-to-action.tpl.php
+++ templates/views-view-fields--call-to-action.tpl.php
@@ -6,20 +6,10 @@
       </div>
       <div class="cta_text-container">
         <h2>
+          <?php dpm($fields); ?>
           <?php print $fields["title"]->content; ?>
         </h2>
       </div>
-<!--      <div class="teaser-slider__item-content-container ">
-        <h2>
-          <?php print $fields["title"]->content; ?>
-        </h2>
-        <p class="teaser-slider__item-text">
-          <?php print $fields['field_kurzer_text']->content; ?>
-        </p>
-        <a href="https://www.robinwood.de" class="button button--window button--is-centered">
-          <?php print $fields["field_button_text"]->content; ?>
-        </a>
-      </div>-->
     </div>
   </a>
 </article>
