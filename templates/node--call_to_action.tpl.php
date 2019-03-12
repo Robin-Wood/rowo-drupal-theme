e8db5c94a403a270cad2889783249fa86e76a7af
Tim Christensen <tim.christensen@robinwood.de>
2019-03-12 17:10:02 +0100

    cta block fixes

--- templates/node--call_to_action.tpl.php
+++ templates/node--call_to_action.tpl.php
@@ -1,22 +1,22 @@
 <article class="cta">
   <a class="outer-link" href="https://www.robinwood.de">
     <div class="cta_container">
-      <?php if (!empty($fields["field_bild"]->content)): ?>
+      <?php if (!empty($content["field_bild"])): ?>
         <div class="cta_image-container">
-          <?php print $fields["field_bild"]->content; ?>
+          <?php print $content["field_bild"]; ?>
         </div>
       <?php endif; ?>
-      <div class="cta_text-container <?php if (!empty($fields["field_bild"]->content)): ?>cta_large<?php endif; ?>">
+      <div class="cta_text-container <?php if (!empty($content["field_bild"])): ?>cta_large<?php endif; ?>">
         <h2>
-          <?php print $fields["title"]->content; ?>
+          <?php print $title; ?>
         </h2>
-        <?php if (!empty($fields["field_bild"]->content)): ?>
+        <?php if (!empty($content["field_bild"])): ?>
           <p>
-            <?php print $fields["field_kurzer_text"]->content; ?>
+            <?php print $content["field_kurzer_text"]; ?>
           </p>
         <?php endif; ?>
         <button class="button button--window button--is-centered">
-          <?php print $fields["field_button_text"]->content; ?>
+          <?php print $content["field_button_text"]; ?>
         </button>
       </div>
     </div>
