ccf7898ac347b443196d3ce6467523fc20a3c499
Tim Christensen <tim.christensen@robinwood.de>
2019-03-12 16:40:41 +0100

    cta block fixes

--- templates/views-view-fields--call-to-action.tpl.php
+++ templates/views-view-fields--call-to-action.tpl.php
@@ -6,13 +6,15 @@
           <?php print $fields["field_bild"]->content; ?>
         </div>
       <?php endif; ?>
-      <div class="cta_text-container">
+      <div class="cta_text-container <?php if (!empty($fields["field_bild"]->content)): ?>cta_large<?php endif; ?>">
         <h2>
           <?php print $fields["title"]->content; ?>
         </h2>
-        <p>
-          <?php print $fields["field_kurzer_text"]->content; ?>
-        </p>
+        <?php if (!empty($fields["field_bild"]->content)): ?>
+          <p>
+            <?php print $fields["field_kurzer_text"]->content; ?>
+          </p>
+        <?php endif; ?>
         <button class="button button--window button--is-centered">
           <?php print $fields["field_button_text"]->content; ?>
         </button>
