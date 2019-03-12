7377e4313c66255f5e1e964590f9431e18912018
Tim Christensen <tim.christensen@robinwood.de>
2019-03-12 16:12:46 +0100

    cta block fixes

--- templates/views-view-fields--call-to-action.tpl.php
+++ templates/views-view-fields--call-to-action.tpl.php
@@ -1,9 +1,11 @@
 <article class="cta">
   <a class="outer-link" href="https://www.robinwood.de">
     <div class="cta_container">
-      <div class="cta_image-container">
-        <?php print $fields["field_bild"]->content; ?>
-      </div>
+      <?php if (!empty($fields["field_bild"])): ?>
+        <div class="cta_image-container">
+          <?php print $fields["field_bild"]->content; ?>
+        </div>
+      <?php endif; ?>
       <div class="cta_text-container">
         <h2>
           <?php print $fields["title"]->content; ?>
