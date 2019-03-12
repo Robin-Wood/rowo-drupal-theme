6a16bbd4e8cb4f3640c28d9f80bc484b1a26ce69
Tim Christensen <tim.christensen@robinwood.de>
2019-03-12 16:08:25 +0100

    cta block fixes

--- templates/node--kampagne.tpl.php
+++ templates/node--kampagne.tpl.php
@@ -63,18 +63,20 @@
         <?php endif; ?>
       </div>
     </div>
-    
-    <?php $block_cta = block_get_blocks_by_region('cta'); ?>
-    <?php if (!empty($block_cta)): ?>
-        <div class="article__cta-block">
-          <div class="row">
-            <div class="col-xs-12">
-              <?php print render($block_cta); ?>
-            </div>
-          </div>
+  </div>
+  
+  <?php $block_cta = block_get_blocks_by_region('cta'); ?>
+  <?php if (!empty($block_cta)): ?>
+    <div class="article__cta-block">
+      <div class="row">
+        <div class="col-xs-12">
+          <?php print render($block_cta); ?>
         </div>
-      <?php endif; ?>
+      </div>
+    </div>
+  <?php endif; ?>
 
+  <div class="row">
     <div class="col-xs-12 col-sm-8 col-sm-offset-2">
       <?php if (!empty($content['field_tags'])): ?>
         <footer class="article__tags-container">
