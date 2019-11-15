9dbe0e348acef94289dc01ff65eb1887a34f8816
Tim Christensen <tim.christensen@robinwood.de>
2019-11-15 12:45:22 +0100

    rename forderungen

--- templates/node--kampagne.tpl.php
+++ templates/node--kampagne.tpl.php
@@ -36,7 +36,7 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2">
         <h2 class="article__subheadline">
-          <?php print $content['field_foderungen']['#title'] ?>
+          <?php print $content['field_titel_forderungen_kriterie'] ?>
         </h2>
 
         <?php print render($content['field_foderungen']); ?>
