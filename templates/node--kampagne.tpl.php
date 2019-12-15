d01954d2e63a1a19eca8d8359fc8e8b6ae433f17
Tim Christensen <tim.christensen@robinwood.de>
2019-12-15 14:27:34 +0100

    altes Forderungen-Feld entfernt

--- templates/node--kampagne.tpl.php
+++ templates/node--kampagne.tpl.php
@@ -32,18 +32,6 @@
       <?php print render($block_text); ?>
   <?php endif; ?>
 
-  <?php if (!empty($content['field_foderungen'])): ?>
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2">
-        <h2 class="article__subheadline">
-          <?php print render($content['field_titel_forderungen_kriterie']); ?>
-        </h2>
-
-        <?php print render($content['field_foderungen']); ?>
-      </div>
-    </div>
-  <?php endif; ?>
-
   <?php if (!empty($content['field_forderungen_kriterien'])): ?>
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2">
