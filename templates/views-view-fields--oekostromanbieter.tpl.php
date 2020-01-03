73874ee63e87d69510b40d4ad3678a2a56c9e32b
Tim Christensen <tim.christensen@robinwood.de>
2020-01-03 13:46:29 +0100

    chart styles

--- templates/views-view-fields--oekostromanbieter.tpl.php
+++ templates/views-view-fields--oekostromanbieter.tpl.php
@@ -10,7 +10,7 @@
       <div class="anbieter__chart chartcontainer" id="chart<?php print $fields['nid']->content;?>"></div>
       <div data-piechart="chart<?php print $fields['nid']->content;?>" class="anbieter__legende">
         <?php print $fields["field_strommix"]->content;?>
-        <h3>Förderbetrag für die Energiewende</h3>
+        <h3 class="anbieter__foerdertitle">Förderbetrag für die Energiewende</h3>
         <p><?php print $fields["field_foerderbetrag"]->content;?></p>
       </div>
     </div>
