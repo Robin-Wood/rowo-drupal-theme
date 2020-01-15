d3d15d534c334ad42693b2323ab233f8dde66d22
Tim Christensen <tim.christensen@robinwood.de>
2020-01-15 18:32:48 +0100

    Akkordeon für Einfache Unterseite

--- templates/node--einfache_unterseite.tpl.php
+++ templates/node--einfache_unterseite.tpl.php
@@ -16,6 +16,11 @@
       <?php endif; ?>
     </div>
   </div>
+  
+  <?php if (!empty($content['field_akkordeon'])): ?>
+    <?php print render($content['field_akkordeon']); ?>
+  <?php endif; ?>
+  
 </article>
 
 <?php
