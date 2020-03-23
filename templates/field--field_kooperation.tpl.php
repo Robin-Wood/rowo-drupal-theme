31da964a64447c536e1cf6f12bc215a1c2e57830
Tim Christensen <tim.christensen@robinwood.de>
2020-03-23 13:46:56 +0100

    untracked files

---
+++ templates/field--field_kooperation.tpl.php
@@ -0,0 +1,15 @@
+<div class="field-name-field-fachreferentin-view">
+  <div class="author">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+        <div class="article__logos">
+          <?php foreach($items as $img): ?>
+            <div class="article__logo">
+              <?php print render($img); ?>
+            </div>
+          <?php endforeach; ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
